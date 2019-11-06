a6e8b5cfc3b69b93cfadbce06630bd908f294e66
chenshuyi <277376753@qq.com>
2019-11-06 20:57:40 +0800

    登录注册页面

---
+++ src/components/Index/Index.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Button } from "antd";
+import axios from 'src/config/axios'
+
+interface IRouter {
+	history: any;
+}
+interface IIndexState {
+    user: any;
+}
+class Index extends React.Component<IRouter,IIndexState> {
+
+	constructor(props: any){
+		super(props)
+		this.state ={
+			user:{}
+		}
+	}
+	async componentWillMount (){
+		await this.getMe()
+	}
+	getMe = async()=>{
+		const response = await axios.get('me')
+		this.setState({user: response.data})
+	}
+	loginOut = ()=>{
+		localStorage.setItem('x-token','')
+		this.props.history.push('/login')
+	}
+
+	render() {
+		return (
+			<div className="Component">
+				<p>欢迎回来，{this.state.user && this.state.user.account}</p>
+				<Button onClick={this.loginOut}>注销</Button>
+			</div>
+		);
+	}
+}
+
+export default Index;
